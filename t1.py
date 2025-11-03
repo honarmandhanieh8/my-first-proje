5adf6b5f5e8185b9900689fe568588fc5423e574
honarmandhanieh8 <honarmandhanieh8@gmail.com>
2025-11-03 19:47:01 +0330

    Create t1.py

---
+++ t1.py
@@ -0,0 +1,5 @@
+name= input("Please enter your name: ")
+birth_year = int(input("Please enter your year of birth: "))
+current_year = 1404
+age = current_year - birth_year
+print(f"Hello, {name}! You are about {age} years old.")
